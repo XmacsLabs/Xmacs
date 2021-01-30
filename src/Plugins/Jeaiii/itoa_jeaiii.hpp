b9e9ca52f52cfa3c1aaff587656bcfbba3ed99e1
Darcy Shen <sadhen@zoho.com>
2021-01-30 11:52:22 +0800

    Xmacs v2020.4 alpha3

--- src/Plugins/Jeaiii/itoa_jeaiii.hpp
+++ src/Plugins/Jeaiii/itoa_jeaiii.hpp
@@ -1,17 +1,21 @@
 
 /******************************************************************************
-* MODULE     : aqua_dialogues.hpp
-* DESCRIPTION: Aqua dialogues widgets
-* COPYRIGHT  : (C) 2007  Massimiliano Gubinelli
+* MODULE     : itoa_jeaiii.hpp
+* COPYRIGHT  : (C) 2019  Darcy Shen
 *******************************************************************************
 * This software falls under the GNU general public license version 3 or later.
 * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
 * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
 ******************************************************************************/
 
-#ifndef AQUA_DIALOGUES_H
-#define AQUA_DIALOGUES_H
+#ifndef ITOA_JEAIII_H
+#define ITOA_JEAIII_H
 
-#include "aqua_widget.h"
+#include <stdint.h>
 
-#endif // defined AQUA_DIALOGUES_H
+char* u32toa_jeaiii (uint32_t u, char* b);
+char* i32toa_jeaiii (int32_t i, char* b);
+char* u64toa_jeaiii (uint64_t n, char* b);
+char* i64toa_jeaiii (int64_t i, char* b);
+
+#endif // ITOA_JEAIII_H
