866390c1fa8a0272125c1f74cac951521919e6bc
沈达 <sadhen@zoho.com>
2021-06-14 20:31:52 +0800

    Open Source in diffs

--- source.sh
+++ source.sh
@@ -2,11 +2,24 @@
 
 FWDIR=$HOME/gitee/xmacs/Xmacs
 
-cd $FWDIR/texmacs
+function clean_source_dir(){
+  echo "==> Cleaning Source Code Directory"
+  cd $FWDIR/texmacs
+  git checkout -- .
+  git clean -df -q
+  rm -rf plugins/quiver
+}
+
+cd $FWDIR
+git submodule update --init --recursive
+
+clean_source_dir
+
+echo "==> Git Apply prepared patches"
 git apply $FWDIR/patches/1.diff
 git apply $FWDIR/patches/2.diff
 
-## Cleaning
+echo "==> Cleaning Unused Code"
 ### Autotools
 rm -rf $FWDIR/texmacs/Makefile.in
 rm -rf $FWDIR/texmacs/aclocal.m4   
@@ -29,7 +42,20 @@ rm -rf $FWDIR/texmacs/src/Plugins/Widkit
 ### Cocoa Related
 rm -rf $FWDIR/texmacs/src/Plugins/Cocoa
 
+## Using the latest Plugins and remove some of them
+rm -rf $FWDIR/texmacs/plugins
+cp -r $FWDIR/plugins $FWDIR/texmacs/
+rm -rf $FWDIR/texmacs/plugins/axiom
+rm -rf $FWDIR/texmacs/plugins/maple
+rm -rf $FWDIR/texmacs/plugins/mathematica
+rm -rf $FWDIR/texmacs/plugins/matlab
+
+echo "==> Overwirte using Xmacs-only files"
 cp -rf $FWDIR/xmacs/ $FWDIR/texmacs/
 
+echo "==> Output to /tmp/Xmacs"
 rm -rf /tmp/Xmacs
 cp -r $FWDIR/texmacs /tmp/Xmacs
+
+clean_source_dir
+
